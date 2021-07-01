35c933ec93fc1884810b0a9b3716b5476a101528
Mihail-Trofimov <dmitriy-ostahov@yandex.ru>
2021-07-01 17:42:47 +1000

    ver-007

--- Assets/MyAsset/Scripts/PlayerMovements.cs
+++ Assets/MyAsset/Scripts/PlayerMovements.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 //эта строчка гарантирует что наш скрипт не завалится 
@@ -5,86 +6,201 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMovements : MonoBehaviour
 {
-    public float Speed = 10f;
-    public float JumpForce = 300f;
+    //public float Speed = 10f;
+    //public float JumpForce = 300f;
 
-    //что бы эта переменная работала добавьте тэг "Ground" на вашу поверхность земли
-    private bool _isGrounded;
+    ////что бы эта переменная работала добавьте тэг "Ground" на вашу поверхность земли
+    //private bool _isGrounded;
     private Rigidbody _rb;
 
+    [Header("Assigned")]
+    [SerializeField] private WheelCollider[] whellCols;
+    //[SerializeField] private GameObject[] whells;
+    [SerializeField] private Transform[] whellMeshs;
+
+    
+    //void whellSteer(float angel)
+    //{
+    //    whellCols[0].steerAngle = angel;
+    //    whellCols[1].steerAngle = angel;
+    //}
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
     }
 
-    // обратите внимание что все действия с физикой 
-    // необходимо обрабатывать в FixedUpdate, а не в Update
     void FixedUpdate()
     {
-        _rb.AddForce(0.0f, 0.0f, Input.GetAxis("Vertical") * Speed);
+        //whellCols[0].steerAngle = 30f * Input.GetAxis("Horizontal");
+        //whellCols[1].steerAngle = 30f * Input.GetAxis("Horizontal");
+        //whellSteer(30f * Input.GetAxis("Horizontal"));
 
-        //Quaternion.RotateTowards(rb.rotation, rotX30, 10.0f * Time.deltaTime)
-        //_rb.MoveRotation(Quaternion.RotateTowards(Input.GetAxis("Horizontal") * Speed, 0.0f, 0.0f));
+        whellCols[0].steerAngle = 30f * Input.GetAxis("Horizontal");
+        whellCols[1].steerAngle = 30f * Input.GetAxis("Horizontal");
 
-        //_rb.MoveRotation(Quaternion.RotateTowards(_rb.rotation, _rb.rotation, _rb.rotation));
-        var angles = _rb.transform.rotation.eulerAngles;
-        angles.z += Input.GetAxis("Horizontal") * Speed;
-        //transform.rotation = Quaternion.Euler(angles);
+        //whellMeshs[0].rotation = Quaternion.Euler(90f, 0f, 30f * Input.GetAxis("Horizontal"));
+        //whellMeshs[1].rotation = Quaternion.Euler(90f, 0f, 30f * Input.GetAxis("Horizontal"));
 
-        _rb.MoveRotation(Quaternion.Euler(angles));
+        //whellMeshs[0].rotation = Quaternion.Euler(90f, 0f, 0f);
+        //whellMeshs[1].rotation = Quaternion.Euler(90f, 0f, 0f);
 
-        //MovementLogic();
-        //JumpLogic();
-    }
 
-    private void MovementLogic()
-    {
-        //float moveHorizontal = Input.GetAxis("Horizontal");
 
-        //float moveVertical = Input.GetAxis("Vertical");
+        //whellMeshs[0].rotation = Quaternion.Euler(90f, 0f, 90f - whellCols[0].steerAngle);
+        //whellMeshs[1].rotation = Quaternion.Euler(90f, 0f, 90f - whellCols[1].steerAngle);
 
-        //Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
 
-        _rb.AddForce(Input.GetAxis("Horizontal") * Speed, 0.0f, Input.GetAxis("Vertical") * Speed );
-        //_rb.AddForce(movement * Speed);
-    }
 
-    private void JumpLogic()
-    {
-        if (Input.GetAxis("Jump") > 0)
+        //Debug.Log(_rb.velocity.magnitude.ToString());
+        
+
+
+        if (Input.GetAxis("Vertical") > 0)
         {
-            if (_isGrounded)
+            if (whellCols[0].motorTorque < 0 || whellCols[1].motorTorque < 0 || whellCols[2].motorTorque < 0 || whellCols[3].motorTorque < 0)
             {
-                _rb.AddForce(Vector3.up * JumpForce);
-
-                // Обратите внимание что я делаю на основе Vector3.up 
-                // а не на основе transform.up. Если персонаж упал или 
-                // если персонаж -- шар, то его личный "верх" может 
-                // любое направление. Влево, вправо, вниз...
-                // Но нам нужен скачек только в абсолютный вверх, 
-                // потому и Vector3.up
+                Debug.Log("Forvard Stop");
+                whellCols[0].brakeTorque = 1000f * Time.deltaTime;
+                whellCols[1].brakeTorque = 1000f * Time.deltaTime;
+                whellCols[2].brakeTorque = 1000f * Time.deltaTime;
+                whellCols[3].brakeTorque = 1000f * Time.deltaTime;
+            }
+            else
+            {
+                Debug.Log("Forvard");
+                whellCols[0].motorTorque = Math.Abs(600f * Input.GetAxis("Vertical")) * Time.deltaTime;
+                whellCols[1].motorTorque = Math.Abs(600f * Input.GetAxis("Vertical")) * Time.deltaTime;
+                whellCols[2].motorTorque = Math.Abs(600f * Input.GetAxis("Vertical")) * Time.deltaTime;
+                whellCols[3].motorTorque = Math.Abs(600f * Input.GetAxis("Vertical")) * Time.deltaTime;
             }
         }
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        IsGroundedUpate(collision, true);
-    }
-
-    void OnCollisionExit(Collision collision)
-    {
-        IsGroundedUpate(collision, false);
-    }
-
-    private void IsGroundedUpate(Collision collision, bool value)
-    {
-        if (collision.gameObject.tag == ("Ground"))
+        else if (Input.GetAxis("Vertical") < 0)
         {
-            _isGrounded = value;
+            if (whellCols[0].motorTorque > 0 || whellCols[1].motorTorque > 0 || whellCols[2].motorTorque > 0 || whellCols[3].motorTorque > 0)
+            {
+                Debug.Log("Back Stop");
+                whellCols[0].brakeTorque = 1000f * Time.deltaTime;
+                whellCols[1].brakeTorque = 1000f * Time.deltaTime;
+                whellCols[2].brakeTorque = 1000f * Time.deltaTime;
+                whellCols[3].brakeTorque = 1000f * Time.deltaTime;
+            }
+            else
+            {
+                Debug.Log("Back");
+                whellCols[0].motorTorque = -1 * Math.Abs(500f * Input.GetAxis("Vertical") * Time.deltaTime);
+                whellCols[1].motorTorque = -1 * Math.Abs(500f * Input.GetAxis("Vertical") * Time.deltaTime);
+                whellCols[2].motorTorque = -1 * Math.Abs(500f * Input.GetAxis("Vertical") * Time.deltaTime);
+                whellCols[3].motorTorque = -1 * Math.Abs(500f * Input.GetAxis("Vertical") * Time.deltaTime);
+            }
         }
+        else
+        {
+            Debug.Log(_rb.velocity.magnitude.ToString());
+            Debug.Log("----");
+            //whellCols[0].brakeTorque = 1f * Time.deltaTime;
+            //whellCols[1].brakeTorque = 1f * Time.deltaTime;
+            //whellCols[2].brakeTorque = 1f * Time.deltaTime;
+            //whellCols[3].brakeTorque = 1f * Time.deltaTime;
+        }
+        //else
+        //{
+
+        //if (Input.GetAxis("Vertical") < 0)
+        //{
+        //    Debug.Log("Revers");
+        //    //whellCols[0].motorTorque = 5f * Input.GetAxis("Vertical");
+        //    //whellCols[1].motorTorque = 5f * Input.GetAxis("Vertical");
+        //    whellCols[2].motorTorque = -15f * Input.GetAxis("Vertical");
+        //    whellCols[3].motorTorque = -15f * Input.GetAxis("Vertical");
+        //}
+        //else
+        //{
+        //Debug.Log("Stop");
+        //whellCols[0].motorTorque = 0f;
+        //whellCols[1].motorTorque = 0f;
+        //whellCols[2].motorTorque = 0f;
+        //whellCols[3].motorTorque = 0f;
+
+        //whellCols[0].brakeTorque = 15f;
+        //whellCols[1].brakeTorque = 15f;
+        //whellCols[2].brakeTorque = 15f;
+        //whellCols[3].brakeTorque = 15f;
+        //}
+
+        //}
+
     }
+
+    
+
+
+
 }
+//_rb.AddForce(0.0f, 0.0f, Input.GetAxis("Vertical") * Speed);
+
+////Quaternion.RotateTowards(rb.rotation, rotX30, 10.0f * Time.deltaTime)
+////_rb.MoveRotation(Quaternion.RotateTowards(Input.GetAxis("Horizontal") * Speed, 0.0f, 0.0f));
+
+////_rb.MoveRotation(Quaternion.RotateTowards(_rb.rotation, _rb.rotation, _rb.rotation));
+//var angles = _rb.transform.rotation.eulerAngles;
+//angles.z += Input.GetAxis("Horizontal") * Speed;
+////transform.rotation = Quaternion.Euler(angles);
+
+//_rb.MoveRotation(Quaternion.Euler(angles));
+
+////MovementLogic();
+////JumpLogic();
+///
+
+
+//    private void MovementLogic()
+//    {
+//        //float moveHorizontal = Input.GetAxis("Horizontal");
+
+//        //float moveVertical = Input.GetAxis("Vertical");
+
+//        //Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+
+//        _rb.AddForce(Input.GetAxis("Horizontal") * Speed, 0.0f, Input.GetAxis("Vertical") * Speed );
+//        //_rb.AddForce(movement * Speed);
+//    }
+
+//    private void JumpLogic()
+//    {
+//        if (Input.GetAxis("Jump") > 0)
+//        {
+//            if (_isGrounded)
+//            {
+//                _rb.AddForce(Vector3.up * JumpForce);
+
+//                // Обратите внимание что я делаю на основе Vector3.up 
+//                // а не на основе transform.up. Если персонаж упал или 
+//                // если персонаж -- шар, то его личный "верх" может 
+//                // любое направление. Влево, вправо, вниз...
+//                // Но нам нужен скачек только в абсолютный вверх, 
+//                // потому и Vector3.up
+//            }
+//        }
+//    }
+
+//    void OnCollisionEnter(Collision collision)
+//    {
+//        IsGroundedUpate(collision, true);
+//    }
+
+//    void OnCollisionExit(Collision collision)
+//    {
+//        IsGroundedUpate(collision, false);
+//    }
+
+//    private void IsGroundedUpate(Collision collision, bool value)
+//    {
+//        if (collision.gameObject.tag == ("Ground"))
+//        {
+//            _isGrounded = value;
+//        }
+//    }
+//}
 
 
 
